5ab5565898206b5d2f6a3dff24e306353da82be5
= <569309786@qq.com>
2018-07-31 23:38:47 +0800

    新增一些文件

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -1,10 +1,14 @@
 // This file was auto created by egg-ts-helper
 // Do not modify this file!!!!!!!!!
 
+import Admin from '../../../app/controller/admin';
 import Home from '../../../app/controller/home';
+import User from '../../../app/controller/user';
 
 declare module 'egg' {
   interface IController {
+    admin: Admin;
     home: Home;
+    user: User;
   }
 }
